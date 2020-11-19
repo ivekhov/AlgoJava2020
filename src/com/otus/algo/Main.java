5d54f0e694f12dbce1ff70f22f1c73b3e9243e48
ivekhov <ivekhov@gmail.com>
2020-11-20 00:18:56 +0300

    Added solution on Harry Potter spells Lesson #01 part 3.

--- src/com/otus/algo/Main.java
+++ src/com/otus/algo/Main.java
@@ -1,6 +1,7 @@
 package com.otus.algo;
 
 
+import com.otus.algo.harry_potter.HarryPotter;
 import com.otus.algo.string_length_0.StringLength;
 import com.otus.algo.tester.Tester;
 import com.otus.algo.tickets_lucky_1_1.LuckyTickets;
@@ -11,9 +12,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        // task #0 String Length
+// Lesson #1. Task #1 String Length
 //        testStringLength();
-        testLuckyTickets();
+
+// Lesson #1. Task #2 String Length
+//        testLuckyTickets();
+
+// Lesson #1. Task #3 Harry Potter Spell
+        guessSpell();
 
     }
     public static void testStringLength() throws IOException {
@@ -28,6 +34,11 @@ public class Main {
         String path = "./data/tickets_1";
         Tester tester = new Tester(task, path);
         tester.runTest();
+    }
+
+    public static void guessSpell() {
+        HarryPotter harryPotter = new HarryPotter();
+        harryPotter.spell();
     }
 
 }
---
+++ src/com/otus/algo/harry_potter/HarryPotter.java
@@ -0,0 +1,34 @@
+package com.otus.algo.harry_potter;
+
+public class HarryPotter {
+
+    public HarryPotter(){ }
+
+    public void spell() {
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 25; j++) {
+                System.out.print(
+                        i < j              // #1
+//                        i == j             // #2
+//                        j == (24 - i)  // #3
+//                        i < (25 - j + 5)   // #4
+//                        i == j - i | i + 1 == j - i  // #5
+//                        (i > 25 / 2 + 3) & (j > 25 / 2 + 3)         // #7
+//                        i == 0 | j == 0    // #8
+//                        (i + 10 < j) | !(i - 10 < j) // #9
+//                        i == 0 + 1 | j == 0 + 1 | i == 24 - 1 | j == 24 - 1 // #11
+//                        (j > (24 - i - 5)) & (j < (24 - i + 5))     // #13
+//                        (i == 0 | j == 0) | (i == 24 | j == 24)   // #19
+//                        i == j | j == (24 - i)   // #24
+
+
+                        ? "# " : ". "
+
+                );
+            }
+            System.out.println();
+
+        }
+    }
+
+}
